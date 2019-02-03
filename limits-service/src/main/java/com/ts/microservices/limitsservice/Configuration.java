e09289a561c81b7bd7ea03ff5ce6d23cf6f95e40
tshaiman <tomer.shaiman@gmail.com>
2019-02-03 11:18:53 +0200

    refactor

--- limits-service/src/main/java/com/ts/microservices/limitsservice/Configuration.java
+++ limits-service/src/main/java/com/ts/microservices/limitsservice/Configuration.java
@@ -2,29 +2,17 @@ package com.ts.microservices.limitsservice;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("limits-service")
+@Data
 public class Configuration {
 
     private int minimum;
     private int maximum;
 
-    public void setMinimum(int minimum) {
-        this.minimum = minimum;
-    }
 
-    public void setMaximum(int maximum) {
-        this.maximum = maximum;
-    }
-
-    public int getMinimum() {
-        return minimum;
-    }
-
-    public int getMaximum() {
-        return maximum;
-    }
 }
--- limits-service/src/main/java/com/ts/microservices/limitsservice/bean/LimitConfiguration.java
+++ limits-service/src/main/java/com/ts/microservices/limitsservice/bean/LimitConfiguration.java
@@ -4,26 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LimitConfiguration {
     private int maximum;
     private int minimum;
 
-    protected LimitConfiguration() {
 
-    }
-
-    public LimitConfiguration(int maximum, int minimum) {
-        super();
-        this.maximum = maximum;
-        this.minimum = minimum;
-    }
-
-    public int getMaximum() {
-        return maximum;
-    }
-
-    public int getMinimum() {
-        return minimum;
-    }
 }
