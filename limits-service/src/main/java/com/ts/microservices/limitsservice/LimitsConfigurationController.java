d05272c017720c6401fa59d82031b3dbc9c5cadf
tshaiman <tomer.shaiman@gmail.com>
2019-02-03 17:39:45 +0200

    hystrix

--- limits-service/src/main/java/com/ts/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/ts/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,7 +1,9 @@
 package com.ts.microservices.limitsservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.ts.microservices.limitsservice.bean.LimitConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,18 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitConfiguration retreiveLimitsFromConfiguration(){
         return  new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+
+    }
+
+    @GetMapping("/fault-tollerant")
+    @HystrixCommand(fallbackMethod = "faultTollerantLimits")
+    public LimitConfiguration retreiveFaultTollerantLimitsFromConfiguration(){
+        throw new RuntimeException("Not Available");
+
+    }
+
+    public LimitConfiguration faultTollerantLimits(){
+        return  new LimitConfiguration(999,9);
 
     }
 }
--- limits-service/src/main/java/com/ts/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/ts/microservices/limitsservice/LimitsServiceApplication.java
@@ -3,11 +3,15 @@ package com.ts.microservices.limitsservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@RefreshScope
+@EnableHystrix
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
